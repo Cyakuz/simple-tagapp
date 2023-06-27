58061643e8d5bde9067c3663449b92f75dd092c4
Cyakuz <102320084+Cyakuz@users.noreply.github.com>
2023-06-28 01:16:18 +0300

    useeffect, usestate, use location ile admin sayfasına props gönderme

--- src/components/Bilesen.js
+++ src/components/Bilesen.js
@@ -1,103 +1,105 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { UrunConsumer } from '../Context';
+import { useLocation } from 'react-router-dom';
 
-class Bilesen extends Component {
-  state = {
-    isVisible: false,
+const Bilesen = ({ urunBilesen, uyari, fiyat }) => {
+  const [isVisible, setIsVisible] = useState(false);
+  const [admintools, setAdmintools] = useState(false);
+  const location = useLocation();
+
+  const toggleVisibility = () => {
+    setIsVisible((prevVisibility) => !prevVisibility);
   };
 
-  toggleVisibility = () => {
-    this.setState((prevState) => ({
-      isVisible: !prevState.isVisible
-    }));
+  const toggleAdminTools = () => {
+    setAdmintools((prevAdmintools) => !prevAdmintools);
   };
 
-  handleKeyDown = (e) => {
+  const handleKeyDown = (e) => {
     if (e.key === 'Enter') {
-      this.toggleVisibility();
+      toggleVisibility();
     }
   };
 
-  render() {
-    const { urunBilesen, uyari, fiyat } = this.props;
-    const { isVisible} = this.state;
+  useEffect(() => {
+    if (location.pathname === '/admin') {
+      setAdmintools(true);
+    } else {
+      setAdmintools(false);
+    }
+  }, [location]);
 
-    return (
-      <UrunConsumer>
-        {(value) => {
-          const { dispatch } = value;
+  return (
+    <UrunConsumer>
+      {(value) => {
+        const { dispatch } = value;
 
-          return (
-            <div>
-              {isVisible ? (
-                <div>
-                  <ul className="flex col-12" style={{ listStyle: 'none' }}>
-                    <li>
-                      <h4>Bileşenler:</h4>
-                      <p className="flex flex-column">{urunBilesen}</p>
-                    </li>
-                    <li>
-                      <h4>Uyarı:</h4>
-                      <p className="flex flex-column">{uyari}</p>
-                    </li>
-                    <li>
-                      <h4>Fiyatı:</h4>
-                      {fiyat}
-                    </li>
-                  </ul>
-                  <ul className="d-flex justify-content-between">
-                    <i
-                      className="fa-solid fa-angle-down"
-                      onClick={this.toggleVisibility}
-                      onKeyDown={this.handleKeyDown}
-                      tabIndex={0}
-                      role="button"
-                      aria-expanded={isVisible}
-                      aria-label="Hide Content"
-                    ></i>
-                    
-                      <i
-                        className="fa-solid fa-trash"
-                        onClick={this.toggleVisibility}
-                        onKeyDown={this.handleKeyDown}
-                        tabIndex={0}
-                        role="button"
-                        aria-expanded={isVisible}
-                        aria-label="Delete Content"
-                      ></i>
-                    
-                  </ul>
-                </div>
-              ) : (
+        return (
+          <div>
+            {isVisible ? (
+              <div>
+                <ul className="flex col-12" style={{ listStyle: 'none' }}>
+                  <li>
+                    <h4>Bileşenler:</h4>
+                    <p className="flex flex-column">{urunBilesen}</p>
+                  </li>
+                  <li>
+                    <h4>Uyarı:</h4>
+                    <p className="flex flex-column">{uyari}</p>
+                  </li>
+                  <li>
+                    <h4>Fiyatı:</h4>
+                    {fiyat}
+                  </li>
+                </ul>
                 <ul className="d-flex justify-content-between">
                   <i
                     className="fa-solid fa-angle-down"
-                    onClick={this.toggleVisibility}
-                    onKeyDown={this.handleKeyDown}
+                    onClick={toggleVisibility}
+                    onKeyDown={handleKeyDown}
                     tabIndex={0}
-                    role="button"
                     aria-expanded={isVisible}
-                    aria-label="Show Content"
+                    aria-label="Hide Content"
                   ></i>
-                  
+                  {admintools && (
                     <i
                       className="fa-solid fa-trash"
-                      onClick={this.toggleVisibility}
-                      onKeyDown={this.handleKeyDown}
+                      onClick={toggleAdminTools}
+                      onKeyDown={handleKeyDown}
                       tabIndex={0}
-                      role="button"
-                      aria-expanded={isVisible}
+                      aria-expanded={admintools}
                       aria-label="Delete Content"
                     ></i>
-                  
+                  )}
                 </ul>
-              )}
-            </div>
-          );
-        }}
-      </UrunConsumer>
-    );
-  }
-}
+              </div>
+            ) : (
+              <ul className="d-flex justify-content-between">
+                <i
+                  className="fa-solid fa-angle-down"
+                  onClick={toggleVisibility}
+                  onKeyDown={handleKeyDown}
+                  tabIndex={0}
+                  aria-expanded={isVisible}
+                  aria-label="Show Content"
+                ></i>
+                {admintools && (
+                  <i
+                    className="fa-solid fa-trash"
+                    onClick={toggleAdminTools}
+                    onKeyDown={handleKeyDown}
+                    tabIndex={0}
+                    aria-expanded={admintools}
+                    aria-label="Delete Content"
+                  ></i>
+                )}
+              </ul>
+            )}
+          </div>
+        );
+      }}
+    </UrunConsumer>
+  );
+};
 
 export default Bilesen;
