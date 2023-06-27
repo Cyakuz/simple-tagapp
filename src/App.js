fee54087cca72eff5a036231288ef181967605aa
Cyakuz <102320084+Cyakuz@users.noreply.github.com>
2023-06-27 03:34:10 +0300

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,12 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 import MenuTabs from './components/MenuTabs';
-import Dolap from './scenes/Dolap';
-import Tezgah from './scenes/Tezgah';
-import Icecekler from './scenes/Icecekler';
-import Tab from 'react-bootstrap/Tab';
-import Tabs from 'react-bootstrap/Tabs';
 import MenuTabs from './components/MenuTabs';
 
 
