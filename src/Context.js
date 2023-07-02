8d5b0f9334d0688f38d62516e56b8610dffcb7d3
Cyakuz <102320084+Cyakuz@users.noreply.github.com>
2023-07-03 01:15:49 +0300

    json server

--- src/Context.js
+++ src/Context.js
@@ -21,36 +21,7 @@ const reducer = (state,action) => {
 
 export class UrunProvider extends Component {
   state = {
-    urunler: [
-      {
-        id: "1",
-        title: "Pasta No: 0",
-        fiyat: "240",
-        urunBilesen: "Fıstık, Kaju",
-        tur: "dolap"
-      },
-      {
-        id: "2",
-        title: "Pasta No: 1",
-        fiyat: "240",
-        urunBilesen: "Süt Ürünleri",
-        tur: "dolap"
-      },
-      {
-        id: "3",
-        title: "Ayran",
-        fiyat: "6",
-        urunBilesen: "Süt Ürünleri",
-        tur: "icecek",
-      },
-      {
-        id: "4",
-        title: "Sakız",
-        fiyat: "8",
-        urunBilesen: "Nane",
-        tur: "tezgah",
-      },
-    ],
+    urunler: [],
       dispatch: action => {
       this.setState(state => reducer(state,action))
     }
